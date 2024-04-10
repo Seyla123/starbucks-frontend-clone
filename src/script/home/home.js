2218aaa16e755fec94a566c8f7faf045ca6d6346
AnB School 0010 <anbschool0010@AnBs-Mac-mini-71.local>
2024-04-10 19:58:29 +0700

    feature : add navbar

--- src/script/home/home.js
+++ src/script/home/home.js
@@ -1,9 +1,9 @@
 // main section
 
-const section1 = document.querySelector(".section-1-layout");
-fetch("../../components/home/section-1.html")
+const content = document.querySelector(".content-layout");
+fetch("../../components/home/content.html")
 .then((res) => res.text())
 .then((data) => {
-  section1.innerHTML = data;
+  content.innerHTML = data;
 })
 .catch((error) => console.error("Error fetching included file:", error));
