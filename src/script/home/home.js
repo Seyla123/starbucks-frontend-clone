6a38cd12925910dce061c0062e36b97824d25930
Seav Seyla <mrrseyla.758@gmail.com>
2024-04-10 23:51:42 +0700

    feature : add something on navbar

--- src/script/home/home.js
+++ src/script/home/home.js
@@ -5,5 +5,15 @@ fetch("../../components/home/content.html")
 .then((res) => res.text())
 .then((data) => {
   content.innerHTML = data;
+  minBtnMenu = document.querySelector("#minBtnMenu");
+  minMenuDropdown = document.querySelector("#minMenuDropdown");
+
+  minBtnMenu.addEventListener("click",function() {
+    minMenuDropdown.classList.add("right-0");
+    alert('hello')
+  });
+
+
 })
 .catch((error) => console.error("Error fetching included file:", error));
+
