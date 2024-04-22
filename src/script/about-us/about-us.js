732d4578022643f5fdd56d55d1b2dc820c654fd0
Sveita <sveitast7@gmail.com>
2024-04-22 13:26:59 +0700

    about-us <our company : add hover to button>

--- src/script/about-us/about-us.js
+++ src/script/about-us/about-us.js
@@ -13,3 +13,11 @@ fetch("../../components/about-us/content-2.html")
     content_about_us_2.innerHTML = data;
   })
   .catch((error) => console.error("Error fetching included file:", error));
+
+  const content_about_us_3 = document.querySelector(".content-3-about-us-layout");
+  fetch("../../components/about-us/content-3.html")
+    .then((res) => res.text())
+    .then((data) => {
+      content_about_us_2.innerHTML = data;
+    })
+    .catch((error) => console.error("Error fetching included file:", error));
