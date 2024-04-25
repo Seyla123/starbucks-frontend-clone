5c0547a2a8be34a9a6d3e74df7f2ee0d7fe478e8
seang makaramarady <seangdy085@gmail.com>
2024-04-25 16:20:14 +0700

    feature : Develop map and Search Bar of Find a store page.

--- src/script/find-a-store/find.js
+++ src/script/find-a-store/find.js
@@ -1,43 +1,212 @@
 // body
-const body = document.querySelector(".body1");
-fetch("../../components/find-a-store/body.html")
-.then((res) => res.text())
+const find = document.querySelector(".find-content-layout");
+fetch("../../components/find-a-store/content.html")
+  .then((res) => res.text())
 
-.then((data) => {
-    body.innerHTML = data;
-})
-.catch((error) => console.error("Error fetching included file:", error));
-//
+  .then((data) => {
+    find.innerHTML = data;
+    // Create array objects
+    const listCards = [
+      {
+        location: "Eden Garden",
+        direction: "Building E, Phnom Penh City Centre, Phnom Penh",
+        distance: "3.6 miles away",
+      },
+      {
+        location: "AEON Mall 1",
+        direction: "3 Samdech Sothearos Blvd, Phnom Penh",
+        distance: "3.3 miles away",
+      },
+      {
+        location: "Prince Phnom Penh Tower",
+        direction: "445 Preah Monivong Blvd, Phnom Penh",
+        distance: "2.7 miles away",
+      },
+      {
+        location: "Lucky Pavilion",
+        direction: "G/F, Soyra Mall, Phnom Penh",
+        distance: "2.8 miles away",
+      },
+      {
+        location: "AEON Mall 1",
+        direction: "3 Samdech Sothearos Blvd, Phnom Penh",
+        distance: "3.3 miles away",
+      },
+      {
+        location: "Sathapana Tower",
+        direction: "41 Preah Norodom Blvd, Phnom Penh",
+        distance: "2.1 miles away",
+      },
+      {
+        location: "BKK Flagship",
+        direction: "#14, St. 57 & 288, Sangkat Boeung Keng Kong",
+        distance: "2.5 miles away",
+      },
+      {
+        location: "TK Royal 1",
+        direction: "G/F & M/F of TkK Royal One, Phnom Penh",
+        distance: "3 miles away",
+      },
+      {
+        location: "Prince Phnom Penh Tower",
+        direction: "445 Preah Monivong Blvd, Phnom Penh",
+        distance: "2.7 miles away",
+      },
+      {
+        location: "Sathapana Tower",
+        direction: "41 Preah Norodom Blvd, Phnom Penh",
+        distance: "2.1 miles away",
+      },
+      {
+        location: "Eden Garden",
+        direction: "Building E, Phnom Penh City Centre, Phnom Penh",
+        distance: "3.6 miles away",
+      },
+      {
+        location: "Toul Tom Pong",
+        direction: "Street 155 at Street 468, Toul Tom Pong I Commune, Phnom Penh",
+        distance: "4.1 miles away",
+      },
+      {
+        location: "The Bridge ",
+        direction: "Village No.14, National Assembly Street, Phnom Penh City",
+        distance: "3.1 miles away",
+      },
+      {
+        location: "Chroy Changva",
+        direction: "Open Space, Prohm Bayon Circle, Sangkat, Phnom Penh",
+        distance: "0.5 miles away",
+      },
+      {
+        location: "TK Royal 1",
+        direction: "G/F & M/F of TkK Royal One, Phnom Penh",
+        distance: "3 miles away",
+      },
+      {
+        location: "Lucky Pavilion",
+        direction: "G/F, Soyra Mall, Phnom Penh",
+        distance: "2.8 miles away",
+      },
+      {
+        location: "AEON Mall 1",
+        direction: "3 Samdech Sothearos Blvd, Phnom Penh",
+        distance: "3.3 miles away",
+      },
+      {
+        location: "BKK Flagship",
+        direction: "#14, St. 57 & 288, Sangkat Boeung Keng Kong",
+        distance: "2.5 miles away",
+      },
+      {
+        location: "The Bridge ",
+        direction: "Village No.14, National Assembly Street, Phnom Penh City",
+        distance: "3.1 miles away",
+      },
+      {
+        location: "Sathapana Tower",
+        direction: "41 Preah Norodom Blvd, Phnom Penh",
+        distance: "2.1 miles away",
+      },
+      {
+        location: "Prince Phnom Penh Tower",
+        direction: "445 Preah Monivong Blvd, Phnom Penh",
+        distance: "2.7 miles away",
+      },
+      {
+        location: "The Bridge ",
+        direction: "Village No.14, National Assembly Street, Phnom Penh City",
+        distance: "3.1 miles away",
+      },
+      {
+        location: "Lucky Pavilion",
+        direction: "G/F, Soyra Mall, Phnom Penh",
+        distance: "2.8 miles away",
+      },
+      {
+        location: "Toul Tom Pong",
+        direction: "Street 155 at Street 468, Toul Tom Pong I Commune, Phnom Penh",
+        distance: "4.1 miles away",
+      },
+      {
+        location: "Eden Garden",
+        direction: "Building E, Phnom Penh City Centre, Phnom Penh",
+        distance: "3.6 miles away",
+      },
+      {
+        location: "TK Royal 1",
+        direction: "G/F & M/F of TkK Royal One, Phnom Penh",
+        distance: "3 miles away",
+      },
+      {
+        location: "Lucky Pavilion",
+        direction: "G/F, Soyra Mall, Phnom Penh",
+        distance: "2.8 miles away",
+      },
+      {
+        location: "AEON Mall 1",
+        direction: "3 Samdech Sothearos Blvd, Phnom Penh",
+        distance: "3.3 miles away",
+      },
 
+    ];
 
-//content1
-const content1 = document.querySelector(".content1");
-fetch("../../components/find-a-store/content1.html")
-.then((res) => res.text())
+    // create cards function to loop over the listlocation
+    const createCardComponent = () => {
+      return `
+      <div id="card-location" class="flex flex-col gap-1">
+      ${listCards.map((card)=> {
+        return `
+        <div 
+        class=" hover:bg-gray-100 rounded-xl py-[20px] flex justify-between items-center">
+        <!-- content -->
+        <div class="pl-10">
+          <h1 class="font-bold text-[14px]">${card.location}</h1>
+          <p class="text-[13px]">${card.direction}</p>
+          <p class="text-[13px]">${card.distance}</p>
+        </div>
+        <!-- button -->
+        <div class="pr-4 pl-10">
+          <!-- heart and problem mark -->
+          <div class="flex justify-end px-2 gap-4">
+            <img
+              class="size-[20px] cursor-pointer"
+              src="../../images/reward/heart.png"
+              alt="" />
+            <img
+              class="rotate-180 size-[20px] cursor-pointer"
+              src="../../images/reward/problem.png"
+              alt="" />
+          </div>
+          <div class="pt-2">
+            <button
+              id="order-btn"
+              class="hover:bg-opacity-90 px-[18px] py-[5px] text-[14px] text-white bg-[#00754a] font-medium border-[1px] rounded-full">
+              Order Here
+            </button>
+          </div>
+        </div>
+      </div>
 
-.then((data) => {
-    content1.innerHTML = data;
-})
-.catch((error) => console.error("Error fetching included file:", error));
-//
+        `
+      }).join("")}
 
-//content2
-const content2 = document.querySelector(".content2");
-fetch("../../components/find-a-store/content2.html")
-.then((res) => res.text())
+      </div>
+    `
+    };
 
-.then((data) => {
-    content2.innerHTML = data;
-})
-.catch((error) => console.error("Error fetching included file:", error));
-// 
+    // create class to link the component in HTML
+    class CardComponent extends HTMLElement {
+      connectedCallback() {
+        this.innerHTML = createCardComponent();
+      }
+    }
+    customElements.define("card-component", CardComponent);
 
-//content3
-const content3 = document.querySelector(".content3");
-fetch("../../components/find-a-store/content3.html")
-.then((res) => res.text())
+    // Create function action
+    const cardStyle = document.querySelectorAll("#card-location")
+    
 
-.then((data) => {
-    content3.innerHTML = data;
-})
-.catch((error) => console.error("Error fetching included file:", error));
+  })
+  .catch((error) => console.error("Error fetching included file:", error));
+// modify card option
+// border-[#00754a] bg-[#e9f0ee] border-2
