0dcf4e005a9cb2367f694523d3267add89435f98
Nem Puthiraksa <raksasparkle@gmail.com>
2024-05-02 14:39:52 +0700

    file : edit navbar

--- src/script/careers/u.s-careers.js
+++ src/script/careers/u.s-careers.js
@@ -5,3 +5,17 @@ fetch("../../components/careers/u.s-content.html")
     u_s_careers.innerHTML = data;
   })
   .catch((error) => console.error("Error fetching included file:", error));
+
+  const whatYourName = document.querySelector("#whatYourName");
+  const whatYourNameInput = document.querySelector("#whatYourNameInput")
+//left-3 -top-2 text-xs bg-white px-1 text-[#00754a] 
+  whatYourNameInput.addEventListener("focus",()=>{
+    whatYourName.classList.add("left-3", "-top-2", "text-xs", "bg-white", "px-1" ,"text-[#00754a]")
+  })
+  whatYourNameInput.addEventListener("focusout",()=>{
+    if(whatYourNameInput.value.length==0){
+      whatYourName.classList.remove("left-3", "-top-2", "text-xs", "bg-white", "px-1" ,"text-[#00754a]")
+    }
+  })
+
+
