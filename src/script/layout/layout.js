3a9bc3c1cb437879bd208452b6510f19f740f124
Seav Seyla <Seav Seyla>
2024-05-02 15:37:20 +0700

    fix : add new footer-full-w-careers and fix padding

--- src/script/layout/layout.js
+++ src/script/layout/layout.js
@@ -2,4 +2,6 @@ import * as navbar from "./navbar.js";
 import * as footer from "./footer.js";
 import * as navbarFullW from "./navbar-full-w.js"
 import * as footerFullW from "./footer-full-w.js"
+import * as footerFullWCareers from "./footer-full-w-careers.js"
 import * as footerPrivacy from "./footer-privacy.js"
+
