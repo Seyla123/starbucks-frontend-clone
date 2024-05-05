982f3efd277b30605f15219ca4e7419f2a5efc6c
Seav Seyla <mrrseyla.758@gmail.com>
2024-05-05 17:09:20 +0700

    feature : add product detail page

--- src/script/menu/menu.js
+++ src/script/menu/menu.js
@@ -8,46 +8,41 @@ fetch('../../components/menu/menu-header.html')
 	.catch((error) => console.error('Error fetching included file:', error));
 
 // menuContent
-const menuContent = document.querySelector('.menu-content-layout');
-fetch('../../components/menu/menu-content.html')
-	.then((res) => res.text())
 
-	.then((data) => {
-		const productBtns = document.querySelectorAll('.productBtn');
-		const productComponents = document.querySelectorAll('.productComponent');
+const productBtns = document.querySelectorAll('.productBtn');
+const productComponents = document.querySelectorAll('.productComponent');
 
-		productComponents.forEach((component) => {
-			if (component.id === 'mainComponent') {
-				component.style.display = 'block';
-			} else {
-				component.style.display = 'none';
-			}
-		});
+productComponents.forEach((component) => {
+	if (component.id === 'mainComponent') {
+		component.style.display = 'block';
+	} else {
+		component.style.display = 'none';
+	}
+});
 
-		productBtns.forEach((btn) => {
-			btn.addEventListener('click', () => {
-				const targetComponentId = btn.getAttribute('data-target');
-				showProductComponent(targetComponentId);
-				hideOtherProductComponents(targetComponentId);
-			});
-		});
+productBtns.forEach((btn) => {
+	btn.addEventListener('click', () => {
+		const targetComponentId = btn.getAttribute('data-target');
+		showProductComponent(targetComponentId);
+		hideOtherProductComponents(targetComponentId);
+	});
+});
 
-		const showProductComponent = (componentId) => {
-			const component = document.getElementById(componentId);
-			console.log(component);
-			if (component) {
-				component.style.display = 'block';
-			}
-		};
+const showProductComponent = (componentId) => {
+	const component = document.getElementById(componentId);
+	console.log(component);
+	if (component) {
+		component.style.display = 'block';
+	}
+};
 
-		const hideOtherProductComponents = (componentIdToShow) => {
-			productComponents.forEach((component) => {
-				if (component.id !== componentIdToShow) {
-					component.style.display = 'none';
-				}
-			});
-		};
+const hideOtherProductComponents = (componentIdToShow) => {
+	productComponents.forEach((component) => {
+		if (component.id !== componentIdToShow) {
+			component.style.display = 'none';
+		}
+	});
+};
 
-		menuContent.innerHTML = data;
-	})
-	.catch((error) => console.error('Error fetching included file:', error));
+
+
