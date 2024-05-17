3e72813c272ab95caec8701a8d96ae86b40016ca
Seav Seyla <Seav Seyla>
2024-05-17 20:11:33 +0700

    feature : login page change to dynamic component

--- src/script/components/layout.js
+++ src/script/components/layout.js
@@ -3,5 +3,6 @@ import { ProductCard , createProductCard } from "./product-card.js";
 import {Sidebar} from './sidebar.js'; 
 import { Navbar } from "./navbar.js";
 import { Footer } from "./footer.js";
+import { InputField } from "./inputComponent.js";
 
 export { createProductCard , createButton };
