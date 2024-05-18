b9512feb8dc13dc581f67540a8a1a8b50a1ca526
NarakCODE <channarakluy@gmail.com>
2024-05-18 18:46:47 +0700

    Changed data array from html to javascript

--- src/script/gift-card/gift-card.js
+++ src/script/gift-card/gift-card.js
@@ -1,6 +1,6 @@
-
-export const createCarousel = (title, imgUrlArray, glide) => {
-   
+import { giftCardDatas } from './data.js';
+console.log(giftCardDatas);
+export const createCarousel = (title, giftCardData, glide) => {
 	return `
     <div class="relative mt-10">
         <!-- title -->
@@ -16,7 +16,7 @@ export const createCarousel = (title, imgUrlArray, glide) => {
             <!-- Slides -->
             <div class="" data-glide-el="track">
                 <ul class="relative w-full overflow-hidden whitespace-no-wrap flex flex-no-wrap [backface-visibility: hidden] [transform-style: preserve-3d] [touch-action: pan-Y] [will-change: transform] py-6">
-                    ${imgUrlArray
+                    ${giftCardData
 											.map((item) => {
 												return `
                             <div class="rounded-lg md:rounded-2xl overflow-hidden shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] hover:-translate-y-4 duration-500">
@@ -42,32 +42,17 @@ export const createCarousel = (title, imgUrlArray, glide) => {
         </div>
     </div>
 `;
-
 };
-let i = 0
+let i = 0;
 export class CarouselComponent extends HTMLElement {
 	connectedCallback() {
 		const title = this.getAttribute('title');
 		const glide = this.getAttribute('glide');
-		// Parse the imgUrl attribute as an array
-		const imgUrlArray = JSON.parse(this.getAttribute('imgUrl'));
+		const dataIndex = parseInt(this.getAttribute('data-index'), 10); 
+		const giftCardData = giftCardDatas[dataIndex]; 
 
-		this.innerHTML = createCarousel(title, imgUrlArray, glide);
-        i++
+		this.innerHTML = createCarousel(title, giftCardData, glide);
+		i++;
 	}
 }
-
 customElements.define('carousel-component', CarouselComponent);
-
-const giftArr = ["../../images/gift-card/feature-1.jpg", "../../images/gift-card/feature-2.jpg", "../../images/gift-card/feature-3.jpg", "../../images/gift-card/feature-4.jpg",
-"../../images/gift-card/feature-5.png", "../../images/gift-card/feature-6.jpg", "../../images/gift-card/feature-7.jpg", "../../images/gift-card/feature-8.png",
-"../../images/gift-card/feature-9.jpg", "../../images/gift-card/feature-10.png", "../../images/gift-card/feature-11.png", "../../images/gift-card/feature-12.jpg",
-"../../images/gift-card/feature-13.png", "../../images/gift-card/feature-14.png", "../../images/gift-card/feature-15.png", "../../images/gift-card/feature-16.jpg",
-"../../images/gift-card/feature-17.png", "../../images/gift-card/feature-18.png", "../../images/gift-card/feature-19.png", "../../images/gift-card/feature-20.jpg"];
-const giftArr2 = ["../../images/gift-card/feature-1.jpg", "../../images/gift-card/feature-2.jpg", "../../images/gift-card/feature-3.jpg", "../../images/gift-card/feature-4.jpg",
-"../../images/gift-card/feature-5.png", "../../images/gift-card/feature-6.jpg", "../../images/gift-card/feature-7.jpg", "../../images/gift-card/feature-8.png",
-"../../images/gift-card/feature-9.jpg", "../../images/gift-card/feature-10.png", "../../images/gift-card/feature-11.png", "../../images/gift-card/feature-12.jpg",
-"../../images/gift-card/feature-13.png", "../../images/gift-card/feature-14.png", "../../images/gift-card/feature-15.png", "../../images/gift-card/feature-16.jpg",
-"../../images/gift-card/feature-17.png", "../../images/gift-card/feature-18.png", "../../images/gift-card/feature-19.png", "../../images/gift-card/feature-20.jpg"];
-
-
