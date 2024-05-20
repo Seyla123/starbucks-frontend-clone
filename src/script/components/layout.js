b50b94e147635a1cad9c9295a7fbdb2314402939
Seav Seyla <Seav Seyla>
2024-05-20 19:14:28 +0700

    fix : fixed join now page , login page

--- src/script/components/layout.js
+++ src/script/components/layout.js
@@ -3,6 +3,5 @@ import { ProductCard , createProductCard } from "./product-card.js";
 import {Sidebar} from './sidebar.js'; 
 import { Navbar } from "./navbar.js";
 import { Footer } from "./footer.js";
-import { InputField } from "./inputComponent.js";
 
 export { createProductCard , createButton };
