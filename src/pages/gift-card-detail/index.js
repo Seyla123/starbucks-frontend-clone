ab8021061f6c4faee5234eb681ff7b3c096f0f54
Seav Seyla <Seav Seyla>
2024-05-21 14:20:51 +0700

    fix : fixed component in card detail page

--- src/pages/gift-card-detail/index.js
+++ src/pages/gift-card-detail/index.js
@@ -1,8 +1,17 @@
 const spanInput = document.querySelectorAll(".spanInput");
 function moveSpan() {
-    for (i = 0; i < spanInput.length; i++) {
+    for (let i = 0; i < spanInput.length; i++) {
         spanInput[i].classList.remove("left-3", "top-[14px]");
         spanInput[i].classList.add("bg-white");
     }
 }
 document.body.addEventListener("onload", moveSpan());
+import "../../script/components/inputTextField.js";
+
+
+const inputField = document.querySelector(".inputField");
+const label = document.querySelector(".label");
+const labelText = label.querySelector("span span");
+const req = document.querySelector(".req");
+
+required(inputField, label, labelText, req);
