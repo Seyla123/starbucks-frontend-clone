041e727f41ec49ce3738302cec75dadc1ff930c5
Nem Puthiraksa <raksasparkle@gmail.com>
2024-05-23 14:46:52 +0700

    file : import career header and footer to layout.js

--- src/script/components/layout.js
+++ src/script/components/layout.js
@@ -5,5 +5,7 @@ import { Navbar } from "./navbar.js";
 import { Footer } from "./footer.js";
 import { ChooseStore } from "./chooseStore.js";
 import { SubNav } from "./subNavbar.js";
+import { CareerFooter } from "./careerFooter.js";
+import { CareerHeader } from "./careerHeader.js";
 
 export { createProductCard , createButton };
