2e9c074f6a4818997f33a6190a2c5f5aadff4a74
Seav Seyla <Seav Seyla>
2024-05-24 17:33:05 +0700

    file : fix menu page

--- src/pages/pickup/pickup.js
+++ src/pages/pickup/pickup.js
@@ -1,7 +1,7 @@
 
 
-const createPickup = (title,text,img,button,btnColor,reverse) => {
-    
+const createPickup = (title, text, img, button, btnColor, reverse) => {
+
     return `
         <div class="py-4 flex flex-col  md:${reverse ? 'flex-row-reverse' : 'flex-row'} mt-4" >
             <div class="w-full md:w-full">
@@ -22,61 +22,60 @@ const createPickup = (title,text,img,button,btnColor,reverse) => {
 
 }
 const pickupCardArr = [{
-title: "Classic Café",
-img: "../../images/order/pickup-image-1.webp",
-text: 'The original, iconic “third place”. A place to sit and stay or order ahead and get on your way. <a class="underline hover:no-underline" href="../../pages/find-a-store/find-a-store.html"> Find a Classic Café</a>, your home away from home.',
-button : false,
+    title: "Classic Café",
+    img: "../../images/order/pickup-image-1.webp",
+    text: 'The original, iconic “third place”. A place to sit and stay or order ahead and get on your way. <a class="underline hover:no-underline" href="../../pages/find-a-store/find-a-store.html"> Find a Classic Café</a>, your home away from home.',
+    button: false,
 
 },
 {
-title: "drive-thru",
-img: "../../images/order/pickup-image-2.webp",
-text: 'Drive up, check out the menu and order from the comfort of your car. You can also order ahead of time on the app—just let the barista know when you pull in.',
-button : false,
+    title: "drive-thru",
+    img: "../../images/order/pickup-image-2.webp",
+    text: 'Drive up, check out the menu and order from the comfort of your car. You can also order ahead of time on the app—just let the barista know when you pull in.',
+    button: false,
 
 },
 {
-title: "Pick Up",
-img: "../../images/order/pickup-image-3.webp",
-text: 'Stores exclusively designed for mobile orders and quick pick up without the wait. Order ahead using the Starbucks app, grab your items and get back to your day!',
-button: "Learn more",
+    title: "Pick Up",
+    img: "../../images/order/pickup-image-3.webp",
+    text: 'Stores exclusively designed for mobile orders and quick pick up without the wait. Order ahead using the Starbucks app, grab your items and get back to your day!',
+    button: "Learn more",
 
 },
 {
-title: "Starbucks Reserve® and Starbucks Reserve® Roastery",
-img: "../../images/order/pickup-image-4.webp",
-text: "can’t-miss experience for anyone with a passion for coffee. Here you'll find exclusive <a class='underline hover:no-underline' href=''>Starbucks Reserve® </a>coffees, signature cocktails, decadent dishes and hands-on experiences.</p> ",
-button : "Learn more",
-btnColor:"bg-white text-",
+    title: "Starbucks Reserve® and Starbucks Reserve® Roastery",
+    img: "../../images/order/pickup-image-4.webp",
+    text: "can’t-miss experience for anyone with a passion for coffee. Here you'll find exclusive <a class='underline hover:no-underline' href=''>Starbucks Reserve® </a>coffees, signature cocktails, decadent dishes and hands-on experiences.</p> ",
+    button: "Learn more",
+    btnColor: "bg-white",
 
 },
 
 {
-title: "Delivery",
-img: "../../images/order/pickup-image-5.webp",
-text: 'Enjoy the ultimate convenience when you have your Starbucks order delivered right to you using our partners like <a class="underline hover:no-underline" href="https://www.doordash.com/sdl/Starbucks/?ignore_splash_experience=true&utm_source=Enterprise&utm_medium=Enterprise&utm_campaign=CX_US_EP_EP_EP_PAR_CUSXXX__+Starbucks_Delivery_Landing_Page_Dec2023">DoorDash</a> and<a class="underline hover:no-underline" href="https://www.ubereats.com/brand/starbucks?&utm_campaign=bd-starbucks-US-evergreendelivery_Owned_landingpage_US&utm_source=starbucks&utm_medium=Web&publisher=Uber%20Eats%20-%20BD%20(Biz%20Dev)%20-%20Partnerships&publisher_sub_campaign_name=bd-starbucks-US-evergreendelivery_Owned_landingpage_US"> Uber Eats*.</a>',
-button : "Learn more",
+    title: "Delivery",
+    img: "../../images/order/pickup-image-5.webp",
+    text: 'Enjoy the ultimate convenience when you have your Starbucks order delivered right to you using our partners like <a class="underline hover:no-underline" href="https://www.doordash.com/sdl/Starbucks/?ignore_splash_experience=true&utm_source=Enterprise&utm_medium=Enterprise&utm_campaign=CX_US_EP_EP_EP_PAR_CUSXXX__+Starbucks_Delivery_Landing_Page_Dec2023">DoorDash</a> and<a class="underline hover:no-underline" href="https://www.ubereats.com/brand/starbucks?&utm_campaign=bd-starbucks-US-evergreendelivery_Owned_landingpage_US&utm_source=starbucks&utm_medium=Web&publisher=Uber%20Eats%20-%20BD%20(Biz%20Dev)%20-%20Partnerships&publisher_sub_campaign_name=bd-starbucks-US-evergreendelivery_Owned_landingpage_US"> Uber Eats*.</a>',
+    button: "Learn more",
 
 },
 {
-title: "Groceries, Airports, Hotels & More",
-img: "../../images/order/pickup-image-6.webp",
-text: 'TWherever your day takes you, find a Starbucks® café on your route. From errands to airports, your next coffee run is just around the corner.',
+    title: "Groceries, Airports, Hotels & More",
+    img: "../../images/order/pickup-image-6.webp",
+    text: 'TWherever your day takes you, find a Starbucks® café on your route. From errands to airports, your next coffee run is just around the corner.',
 
 },
 ]
 class PickUpCard extends HTMLElement {
-constructor() {
-    super()
-}
-connectedCallback() {
-    let isRevers = false;
-    pickupCardArr.map((item)=>{
-        this.innerHTML += createPickup(item.title,item.text,item.img,item.button,item.btnColor,isRevers)
-        isRevers = !isRevers
-    })
-    
-}
+    constructor() {
+        super()
+    }
+    connectedCallback() {
+        let isRevers = false;
+        pickupCardArr.map((item) => {
+            this.innerHTML += createPickup(item.title, item.text, item.img, item.button, item.btnColor, isRevers)
+            isRevers = !isRevers
+        })
+    }
 }
 
 customElements.define('pickup-card', PickUpCard);
