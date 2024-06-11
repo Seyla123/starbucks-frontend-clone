a7fe6962524be17a714c154029b66f9142b44adc
Seav <Mrrseyla.758@gmail.com>
2024-06-11 20:30:38 +0700

    file : add item array , category , subcategory

---
+++ src/pages/menu/subCategoryArr.js
@@ -0,0 +1,100 @@
+export const subCategory = [
+    {
+        "sub_category_id": 0,
+        "category_id": 0,
+        "sub_category_name": "Hot Coffees",
+        "sub_category_img_path": "1.png",
+    },
+    {
+        "sub_category_id": 1,
+        "category_id": 0,
+        "sub_category_name": "Cold Coffees",
+        "sub_category_img_path": "5.png",
+    },
+    {
+        "sub_category_id": 2,
+        "category_id": 0,
+        "sub_category_name": "Starbucks Refreshers Beverages",
+        "sub_category_img_path": "p10.jpg",
+    },
+    {
+        "sub_category_id": 3,
+        "category_id": 0,
+        "sub_category_name": "Frappuccino Blended Beverages",
+        "sub_category_img_path": "p9.jpg",
+    },
+    {
+        "sub_category_id": 4,
+        "category_id": 1,
+        "sub_category_name": "Hot Breakfast",
+        "sub_category_img_path": "food1.png",
+    },
+    {
+        "sub_category_id": 5,
+        "category_id": 1,
+        "sub_category_name": "Oatmeal & Yogurt",
+        "sub_category_img_path": "food2.png",
+    },
+    {
+        "sub_category_id": 6,
+        "category_id": 1,
+        "sub_category_name": "Bakery",
+        "sub_category_img_path": "food5.png",
+    },
+    {
+        "sub_category_id": 7,
+        "category_id": 1,
+        "sub_category_name": "Lunch",
+        "sub_category_img_path": "food1.png",
+    },
+    {
+        "sub_category_id": 8,
+        "category_id": 1,
+        "sub_category_name": "Snacks & Sweets",
+        "sub_category_img_path": "food4.png",
+    },
+    {
+        "sub_category_id": 9,
+        "category_id": 2,
+        "sub_category_name": "Whole bean",
+        "sub_category_img_path": "whole-bean-img/p6.jpg",
+    },
+    {
+        "sub_category_id": 10,
+        "category_id": 2,
+        "sub_category_name": "VIA® Instant",
+        "sub_category_img_path": "p01.jpg",
+    },
+    {
+        "sub_category_id": 11,
+        "category_id": 3,
+        "sub_category_name": "Cold Cups",
+        "sub_category_img_path": "cold-cup-img/p1.jpg",
+    },
+    {
+        "sub_category_id": 12,
+        "category_id": 3,
+        "sub_category_name": "Tumblers",
+        "sub_category_img_path": "tumblers-img/tumblers.jpg",
+    },
+    {
+        "sub_category_id": 13,
+        "category_id": 3,
+        "sub_category_name": "Mugs",
+        "sub_category_img_path": "mugs.jpg",
+    },
+    {
+        "sub_category_id": 14,
+        "category_id": 3,
+        "sub_category_name": "Water Bottles",
+        "sub_category_img_path": "water.jpg",
+    },
+    {
+        "sub_category_id": 14,
+        "category_id": 3,
+        "sub_category_name": "Others",
+        "sub_category_img_path": "other.jpg",
+    },
+
+    
+]
